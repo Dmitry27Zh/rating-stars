bcaaeac2b896f34d0b0e1c05937a7be57726ffb5
Dmitry <dmitry27zh@gmail.com>
2022-05-31 22:09:48 +0300

    Дорабатывает скрипт

--- js/modules/rating.js
+++ js/modules/rating.js
@@ -2,11 +2,9 @@ const elements = document.querySelectorAll('.rating__bar')
 
 const init = (bar) => {
   const stars = bar.querySelectorAll('.rating__star')
-  let isRatingBarActive = false;
 
   const ratingBarChangeHandler = () => {
     bar.classList.add('rating__bar--active')
-    isRatingBarActive = true
     bar.removeEventListener('change', ratingBarChangeHandler)
   }
 
@@ -15,14 +13,10 @@ const init = (bar) => {
   for (let star of stars) {
     star.addEventListener('mouseenter', () => {
       bar.classList.add('rating__bar--hover')
-      bar.classList.remove('rating__bar--active')
     })
 
     star.addEventListener('mouseleave', () => {
       bar.classList.remove('rating__bar--hover');
-      if (isRatingBarActive) {
-        bar.classList.add('rating__bar--active')
-      }
     })
   }
 }
